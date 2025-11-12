d46b1521ab687b99ae5025a8ac94d767558258ab
Mariogarber <mario.garcia.berenguer@gmail.com>
2025-11-12 17:47:27 +0100

    delete all

--- volumenes/graphdb_data/data/settings.js
+++ volumenes/graphdb_data/data/settings.js
@@ -1,5 +1,99 @@
 {
   "properties" : {
     "current.location" : ""
+  },
+  "import.local" : {
+    "GESTDB;;category.ttl" : {
+      "name" : "category.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "http:gestdb/graph/category",
+      "replaceGraphs" : [ ],
+      "baseURI" : "http:gestdb/graph/category",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "77fa7c3e-f8ae-4a11-9f19-e5310bf67a95",
+      "timestamp" : 1762944229339,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "GESTDB;;comments.ttl" : {
+      "name" : "comments.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in 3s.",
+      "context" : "http:gestdb/graph/comments",
+      "replaceGraphs" : [ ],
+      "baseURI" : "http:gestdb/graph/comments",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "e8493794-cb6a-4d2f-a7b1-f20b2dbfbbd4",
+      "timestamp" : 1762944229564,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "GESTDB;;videos.ttl" : {
+      "name" : "videos.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in 1s.",
+      "context" : "http:gestdb/graph/videos",
+      "replaceGraphs" : [ ],
+      "baseURI" : "http:gestdb/graph/videos",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "e5735d57-0543-439f-8e1c-c7bbf6697ca8",
+      "timestamp" : 1762951237851,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    },
+    "GESTDB;;channels.ttl" : {
+      "name" : "channels.ttl",
+      "status" : "DONE",
+      "message" : "Imported successfully in less than a second.",
+      "context" : "http:gestdb/graph/channels",
+      "replaceGraphs" : [ ],
+      "baseURI" : "http:gestdb/graph/channels",
+      "forceSerial" : false,
+      "type" : "file",
+      "format" : null,
+      "data" : "247ba570-9896-429f-9bb7-8b76c50c4bd5",
+      "timestamp" : 1762963232647,
+      "parserSettings" : {
+        "preserveBNodeIds" : false,
+        "failOnUnknownDataTypes" : false,
+        "verifyDataTypeValues" : false,
+        "normalizeDataTypeValues" : false,
+        "failOnUnknownLanguageTags" : false,
+        "verifyLanguageTags" : true,
+        "normalizeLanguageTags" : false,
+        "stopOnError" : true
+      }
+    }
   }
 }
